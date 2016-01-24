fe1b6e0b823f7ed0bb33ea5240ed2a49ccc204e6
sourav verma <95sourav_v@zoho.com>
2016-01-25 01:30:02 +0530

    Day 1: Data Types!

---
+++ Day 1: Data Types!.cpp
@@ -0,0 +1,26 @@
+/* Sourav verma IPG_2013108
+   ABV-IIITM , Gwalior  */
+   
+   
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    cout<<"Primitive "<<":"<< " double"<<"\n";
+    cout<<"Primitive "<<":"<< " char"<<"\n";
+    cout<<"Primitive "<<":"<< " boolean"<<"\n";
+    cout<<"Primitive "<<":"<< " int"<<"\n";
+    cout<<"Referenced "<<":"<< " String"<<"\n";
+    cout<<"Primitive "<<":"<< " boolean"<<"\n";
+    cout<<"Primitive "<<":"<< " double"<<"\n";
+    cout<<"Primitive "<<":"<< " char"<<"\n";
+    cout<<"Referenced "<<":"<< " String"<<"\n";
+    
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
