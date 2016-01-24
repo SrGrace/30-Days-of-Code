cc347fc6c597695cfedeb0e5b50a16e6f967745c
sourav verma <95sourav_v@zoho.com>
2016-01-25 01:39:26 +0530

    Day 6: Let's Review!

---
+++ Day 6: Let's Review!.cpp
@@ -0,0 +1,44 @@
+/*  Sourav_Verma  IPG_2013108 
+    ABV-IIITM , Gwalior  */
+    
+    
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+        }
+        for(int k=0;k<=i;k++){
+            cout<<"#";
+        }
+        cout<<"\n";
+    }     
+    return 0;
+}
