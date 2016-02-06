acb03b95ac7e2329108bef6332c55e773f217eba
sourav verma <95sourav_v@zoho.com>
2016-02-06 20:32:41 +0530

    Day 27: Testing Part II!.cpp

---
+++ Day 27: Testing Part II!.cpp
@@ -0,0 +1,34 @@
+/* sourav verma IPG_2013108
+   ABV-IIITM,Gwalior  */
+   
+   
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    cout<<"5 \n";
+    cout<<"3 3 \n";
+    cout<<"-1 0 6 \n";
+
+    cout<<"9 2 \n";
+    cout<<"-1 0 1 2 3 4 5 6 7 \n";
+
+    cout<<"5 4 \n";
+    cout<<"-5 0 5 8 7 \n";
+
+    cout<<"8 4 \n";
+    cout<<"-3 0 -7 -9 7 66 999 89 \n";
+
+    cout<<"4 4 \n";
+    cout<<"-1 0 8 90 \n";
+    
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
+
+
